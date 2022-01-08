4365546f5adce987fffcb70cd025738d5cf3d550
Joe <joehuang0817@gmail.com>
2022-01-08 17:06:55 +0800

    Revert "test 4"
    This reverts commit 160593b5a5ec91345239a4a9ec12810dbae96643.

--- src/main/java/app/TestApp.java
+++ src/main/java/app/TestApp.java
@@ -3,6 +3,8 @@ package app;
 public class TestApp {
 	
 	public static void main(String[] args) {
+		System.out.println("Hi Git!");
 		System.out.println("Hi Roy yoyoyoyoyoyoyoyoyo!");
+		System.out.println("333!");
 	}
 }
