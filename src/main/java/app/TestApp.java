281ceb9e48770ab866df0b3734d04952c2cb523b
Roy Chiang <m03272002@gmail.com>
2022-01-08 17:01:20 +0800

    3 time

---
+++ src/main/java/app/TestApp.java
@@ -0,0 +1,10 @@
+package app;
+
+public class TestApp {
+	
+	public static void main(String[] args) {
+		System.out.println("Hi Git!");
+		System.out.println("Hi Roy yoyoyoyoyoyoyoyoyo!");
+		System.out.println("333!");
+	}
+}
