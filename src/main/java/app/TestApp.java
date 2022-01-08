160593b5a5ec91345239a4a9ec12810dbae96643
Joe <joehuang0817@gmail.com>
2022-01-08 17:06:33 +0800

    test 4

--- src/main/java/app/TestApp.java
+++ src/main/java/app/TestApp.java
@@ -3,8 +3,6 @@ package app;
 public class TestApp {
 	
 	public static void main(String[] args) {
-		System.out.println("Hi Git!");
 		System.out.println("Hi Roy yoyoyoyoyoyoyoyoyo!");
-		System.out.println("333!");
 	}
 }
