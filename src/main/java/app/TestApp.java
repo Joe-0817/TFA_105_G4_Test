26113e91e51b7ee5e2c4c7c1a8be01e0d7ab94f2
Joe <joehuang0817@gmail.com>
2022-01-09 10:39:53 +0800

    app fix

--- src/main/java/app/TestApp.java
+++ src/main/java/app/TestApp.java
@@ -4,10 +4,7 @@ public class TestApp {
 	
 	public static void main(String[] args) {
 		System.out.println("Hi Roy yoyoyoyoyoyoyoyoyo!");
-<<<<<<< HEAD
 		System.out.println("Hi Gitttttt!");
-=======
 		System.out.println("Branch!");
->>>>>>> new_B1
 	}
 }
